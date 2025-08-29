7f6f451cafb1502ae32a708d7176807b8dde8c90
Th0u305 <ahnaf4411@gmail.com>
2025-08-30 05:06:20 +0600

    removed custom opt html template

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -1,63 +1,54 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import ejs from "ejs";
 import nodemailer from "nodemailer";
-import path from "path";
 import { envVars } from "../config/env";
 import AppError from "../errorHelper/AppError";
 
 const transporter = nodemailer.createTransport({
-    port: Number(envVars.SMTP_PORT),
-    secure: false,
-    service : "gmail" ,
-    auth: {
-        user: envVars.SMTP_USER,
-        pass: envVars.SMTP_PASS
-    },
-    
-})
-
+  port: Number(envVars.SMTP_PORT),
+  secure: false,
+  service: "gmail",
+  auth: {
+    user: envVars.SMTP_USER,
+    pass: envVars.SMTP_PASS,
+  },
+});
 
 interface SendEmailOptions {
-    to: string,
-    subject: string;
-    templateName: string;
-    templateData?: Record<string, any>
-    attachments?: {
-        filename: string,
-        content: Buffer | string,
-        contentType: string
-    }[]
+  to: string;
+  subject: string;
+  templateName: string;
+  templateData?: Record<string, any>;
+  attachments?: {
+    filename: string;
+    content: Buffer | string;
+    contentType: string;
+  }[];
 }
 
 export const sendEmail = async ({
-    to,
-    subject,
-    templateName,
-    templateData,
-    attachments
+  to,
+  subject,
+  templateData,
+  attachments,
 }: SendEmailOptions) => {
-    try {
-        const templatePath = path.join(__dirname, `templates/${templateName}.ejs`)
-        
-        const html = await ejs.renderFile(templatePath, templateData)
-            // const html = `<h1>Hello!</h1><p>Your OTP is: ${templateData?.otp} </p>
-            // <p> This code is only valid for 2 minute </p>`;
-            
-        await transporter.sendMail({
-            from: envVars.SMTP_FROM,
-            to: to,
-            subject: subject,
-            html: html,
-            attachments: attachments?.map(attachment => ({
-                filename: attachment.filename,
-                content: attachment.content,
-                contentType: attachment.contentType
-            }))
-        })
+  try {
+    const html = `<h1>Hello!</h1><p>Your OTP is: ${templateData?.otp} </p>
+            <p> This code is only valid for 2 minute </p>`;
+
+    await transporter.sendMail({
+      from: envVars.SMTP_FROM,
+      to: to,
+      subject: subject,
+      html: html,
+      attachments: attachments?.map((attachment) => ({
+        filename: attachment.filename,
+        content: attachment.content,
+        contentType: attachment.contentType,
+      })),
+    });
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (error: any) {
-        throw new AppError(401, "Email error")
-    }
-
-}
+  } catch (error: any) {
+    throw new AppError(401, "Email error");
+  }
+};
